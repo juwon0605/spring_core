b93c5f7f316787988467790eca5ffecda56bcc5e
juwon <58173061+juwon0605@users.noreply.github.com>
2021-08-18 00:05:56 +0900

    회원 도메인 테스트

---
+++ src/main/java/spring/spring_core/MemberApp.java
@@ -0,0 +1,19 @@
+package spring.spring_core;
+
+import spring.spring_core.member.Grade;
+import spring.spring_core.member.Member;
+import spring.spring_core.member.MemberService;
+import spring.spring_core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findeMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findeMember.getName());
+    }
+}
