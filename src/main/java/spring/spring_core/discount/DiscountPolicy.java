1b854e9a8d48bb78d70c2d81e54203752cd0bdd6
juwon <58173061+juwon0605@users.noreply.github.com>
2021-08-18 00:47:44 +0900

    주문과 할인 도메인 개발

---
+++ src/main/java/spring/spring_core/discount/DiscountPolicy.java
@@ -0,0 +1,13 @@
+package spring.spring_core.discount;
+
+import spring.spring_core.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     *
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
+
---
+++ src/main/java/spring/spring_core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package spring.spring_core.order;
+
+import spring.spring_core.discount.DiscountPolicy;
+import spring.spring_core.discount.FixDiscountPolicy;
+import spring.spring_core.member.Member;
+import spring.spring_core.member.MemberRepository;
+import spring.spring_core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
