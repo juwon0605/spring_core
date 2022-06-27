09def44bebbff0c8da5eb775a3ffc27910bdcc3a
Juwon Park <devprofessionalism@gmail.com>
2022-06-27 16:58:01 +0900

    AppConfig 리택터링

--- src/main/java/spring/spring_core/AppConfig.java
+++ src/main/java/spring/spring_core/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.spring_core;
 
+import spring.spring_core.discount.DiscountPolicy;
 import spring.spring_core.discount.FixDiscountPolicy;
+import spring.spring_core.member.MemberRepository;
 import spring.spring_core.member.MemberService;
 import spring.spring_core.member.MemberServiceImpl;
 import spring.spring_core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import spring.spring_core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
