92fc3bcfef63a0134bdf6853baa303bf2bc094b8
Juwon Park <devprofessionalism@gmail.com>
2022-07-11 21:18:33 +0900

    :art: 캠퍼스 핵데이 Java 코딩 컨벤션 적용

--- src/test/java/spring/spring_core/member/MemberServiceTest.java
+++ src/test/java/spring/spring_core/member/MemberServiceTest.java
@@ -3,30 +3,31 @@ package spring.spring_core.member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import spring.spring_core.AppConfig;
 
 public class MemberServiceTest {
 
-//    MemberService memberService = new MemberServiceImpl();
-    MemberService memberService;
+	//    MemberService memberService = new MemberServiceImpl();
+	MemberService memberService;
 
-//  Test 실행 전 매번 의존관계 주입
-    @BeforeEach
-    public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
-    }
+	//  Test 실행 전 매번 의존관계 주입
+	@BeforeEach
+	public void beforeEach() {
+		AppConfig appConfig = new AppConfig();
+		memberService = appConfig.memberService();
+	}
 
-    @Test
-    void join() {
-        //given
-        Member member = new Member(1L, "memberA", Grade.VIP);
+	@Test
+	void join() {
+		//given
+		Member member = new Member(1L, "memberA", Grade.VIP);
 
-        //when
-        memberService.join(member);
-        Member findMember = memberService.findMember(1L);
+		//when
+		memberService.join(member);
+		Member findMember = memberService.findMember(1L);
 
-        //then
-        Assertions.assertThat(member).isEqualTo(findMember);
-    }
+		//then
+		Assertions.assertThat(member).isEqualTo(findMember);
+	}
 }
--- src/test/java/spring/spring_core/order/OrderServiceTest.java
+++ src/test/java/spring/spring_core/order/OrderServiceTest.java
@@ -3,35 +3,35 @@ package spring.spring_core.order;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import spring.spring_core.AppConfig;
 import spring.spring_core.member.Grade;
 import spring.spring_core.member.Member;
 import spring.spring_core.member.MemberService;
-import spring.spring_core.member.MemberServiceImpl;
 
 public class OrderServiceTest {
 
-//    MemberService memberService = new MemberServiceImpl();
-//    OrderService orderService = new OrderServiceImpl(memberRepository, discountPolicy);
+	//    MemberService memberService = new MemberServiceImpl();
+	//    OrderService orderService = new OrderServiceImpl(memberRepository, discountPolicy);
 
-    private MemberService memberService;
-    private OrderService orderService;
+	private MemberService memberService;
+	private OrderService orderService;
 
-    //  Test 실행 전 매번 의존관계 주입
-    @BeforeEach
-    public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
-        orderService = appConfig.orderService();
-    }
+	//  Test 실행 전 매번 의존관계 주입
+	@BeforeEach
+	public void beforeEach() {
+		AppConfig appConfig = new AppConfig();
+		memberService = appConfig.memberService();
+		orderService = appConfig.orderService();
+	}
 
-    @Test
-    void createOrder(){
-        Long memberId = 1L;
-        Member member = new Member(memberId, "memberA", Grade.VIP);
-        memberService.join(member);
+	@Test
+	void createOrder() {
+		Long memberId = 1L;
+		Member member = new Member(memberId, "memberA", Grade.VIP);
+		memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
-        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
-    }
+		Order order = orderService.createOrder(memberId, "itemA", 10000);
+		Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+	}
 }
