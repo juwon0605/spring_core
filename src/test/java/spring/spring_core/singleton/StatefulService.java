c11e1d3e868b7cd8d7b490256cfc269fb548eb84
Juwon Park <devprofessionalism@gmail.com>
2022-07-18 13:06:58 +0900

    싱글톤 방식의 주의점

---
+++ src/test/java/spring/spring_core/singleton/StatefulService.java
@@ -0,0 +1,20 @@
+package spring.spring_core.singleton;
+
+public class StatefulService {
+
+	// private int price; //상태를 유지하는 필드
+
+	// public void order(String name, int price) {
+	// 	System.out.println("name = " + name + " price = " + price);
+	// 	this.price = price; //여기가 문제!
+	// }
+
+	// public int getPrice() {
+	// 	return price;
+	// }
+
+	public int order(String name, int price) {
+		System.out.println("name = " + name + " price = " + price);
+		return price; //이렇게 해결!
+	}
+}
